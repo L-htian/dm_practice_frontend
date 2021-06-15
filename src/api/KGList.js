8284c464473d5246f7316ef22a4b7ccd0d43938c
李昊天 <1425112581@qq.com>
2021-06-15 10:00:33 +0800

    feat: API添加注释

--- src/api/KGList.js
+++ src/api/KGList.js
@@ -4,6 +4,12 @@ const api = {
     KGListPre: 'http://localhost:8089/api/KGList'
 }
 
+/**
+ * 获得图的列表信息
+ * @return 图的列表信息
+ * @author 吴昊宇
+ * @date 2021/3/29
+ */
 export function getAllGraphAPI() {
     let graphs = []
     $.ajax(`${api.KGListPre}/getAllGraph`, {
@@ -20,6 +26,13 @@ export function getAllGraphAPI() {
     return graphs
 }
 
+/**
+ * 删除图谱
+ * @param graphId 图谱id
+ * @return None
+ * @author 吴昊宇
+ * @date 2021/3/29
+ */
 export function deleteGraphAPI(graphId) {
     $.ajax(`${api.KGListPre}/${graphId}/deleteGraph`, {
         type: 'POST',
@@ -30,6 +43,13 @@ export function deleteGraphAPI(graphId) {
     })
 }
 
+/**
+ * 更新图谱
+ * @param graph 图谱
+ * @return None
+ * @author 吴昊宇
+ * @date 2021/3/29
+ */
 export function updateGraphAPI(graph) {
     $.ajax(`${api.KGListPre}/updateGraph`, {
         type: 'POST',
@@ -43,7 +63,13 @@ export function updateGraphAPI(graph) {
     })
 }
 
-
+/**
+ * 图谱融合
+ * @param graphIdList 图谱id的列表
+ * @return 融合之后的图谱信息
+ * @author 李昊天
+ * @date 2021/6/11
+ */
 export function fuseGraphAPI(graphIdList) {
     let re
     $.ajax(`${api.KGListPre}/fuseGraph`, {
